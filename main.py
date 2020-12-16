38edb467c89c835739713839d55b28bd4dfa01c7
Roi Israeli <roiisra4@gmail.com>
2020-12-16 11:08:56 +0200

    Backend initialize

---
+++ main.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello"
+
+@app.route('/roi')
+def roi():
+    return "Roi"
+
+@app.route('/daniel')
+def daniel():
+    return "Daniel"
+
+app.run(port=5000, debug=True)
+
