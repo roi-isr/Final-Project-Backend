c3aaf2f07aa73675acecf6ae0e7760ebe43ebfc1
Roi Israeli <64132493+roi-isr@users.noreply.github.com>
2020-12-29 20:13:35 +0200

    Delete connection_config.py

--- connection_config.py
+++
@@ -1,9 +0,0 @@
-""" Connection configuration file """
-
-CONNECTION_INFO = """
-        dbname='d6sja18bu5d6qh'
-        user='srmngikqacnqlj'
-        password='c74259d2a98acc497a45815abd47dfd4cf0d29484019b547d1c1b8fe789f9fd2'
-        host='ec2-54-170-100-209.eu-west-1.compute.amazonaws.com'
-        port='5432'
-        """
