38d095f79dd7000523d2565f41bfe8d93b39a27d
Roi Israeli <roiisra4@gmail.com>
2021-01-07 11:29:07 +0200

    added heroku env config for DATABASE_URL

--- connection_config.py
+++ connection_config.py
@@ -1,8 +1,8 @@
 """ Connection configuration file """
 import os
-from connection_secret import CONNECTION_SECRET
-
-
-CONNECTION_INFO = os.environ['DATABASE_URL']
-if not CONNECTION_INFO:
+CONNECTION_INFO = ""
+try:
+    from connection_secret import CONNECTION_SECRET
     CONNECTION_INFO = CONNECTION_SECRET
+except ModuleNotFoundError:
+    CONNECTION_INFO = os.environ['DATABASE_URL']
