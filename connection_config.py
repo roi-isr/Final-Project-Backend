f82f2e7d8e847d9c8d424d5e8f0dca64daea1ccb
Roi Israeli <roiisra4@gmail.com>
2021-01-07 10:59:56 +0200

    seperate secret information from github

--- connection_config.py
+++ connection_config.py
@@ -1,9 +1,8 @@
 """ Connection configuration file """
+import os
+from connection_secret import CONNECTION_SECRET
 
-CONNECTION_INFO = """
-        dbname='d6sja18bu5d6qh'
-        user='srmngikqacnqlj'
-        password='c74259d2a98acc497a45815abd47dfd4cf0d29484019b547d1c1b8fe789f9fd2'
-        host='ec2-54-170-100-209.eu-west-1.compute.amazonaws.com'
-        port='5432'
-        """
+
+CONNECTION_INFO = os.environ['DATABASE_URL']
+if not CONNECTION_INFO:
+    CONNECTION_INFO = CONNECTION_SECRET
