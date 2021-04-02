9b3cf3b5800a531ca97c1ab466705cbcca1ba9cb
Roi Israeli <roiisra4@gmail.com>
2021-04-02 03:07:21 +0300

    Working on ML

--- server/ML/ML_main.py
+++ server/ML/ML_main.py
@@ -8,6 +8,7 @@ from server.ML.price_advise_regression import \
     build_and_test_regression_models as build_advise_regression, \
     make_predictions as make_advise_predictions
 from server.ML.Storage.model_storage import ModelStorage
+import math
 
 
 def build_ml_models():
@@ -27,14 +28,16 @@ def build_ml_advise_models():
 def exec_predictions(data_list):
     # Make some predictions (up to the user) over the best selected model
     # try:
+    num_of_items = ModelStorage.count_advise_items()
     best_main_regression_model, scalers_main = ModelStorage.get_main_model_from_db()
     predicted_price = make_diamond_predictions(best_main_regression_model, scalers_main, data_list)
-    if ModelStorage.count_advise_items() > 7:
+    if num_of_items > 7:
         best_advise_regression_model, scalers_advise, accuracy = ModelStorage.get_advise_model_from_db()
         print(F'---------COUNTER: {ModelStorage.count_advise_items()}, PRED: {accuracy}---------------')
         predicted_advise_price = make_advise_predictions(best_advise_regression_model, scalers_advise, data_list)
         if accuracy > 0:
-            admin_expertise = accuracy
+            experience_rank = (math.atan(0.05 * num_of_items)/math.pi) * 2
+            admin_expertise = accuracy * experience_rank
         else:
             admin_expertise = 0
         final_pred = (admin_expertise * predicted_advise_price) + ((1 - admin_expertise) * predicted_price)
